0ceabbc432438a47c53973d437c3dcb065e9ae08
IMaeots <indrek160@gmail.com>
2024-12-11 13:16:24 +0200

    creating a game in web functionality

--- TicTacTwo/WebApp/Pages/Play/Index.cshtml.cs
+++ TicTacTwo/WebApp/Pages/Play/Index.cshtml.cs
@@ -1,13 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace WebApp.Pages.Play;
-
-public class PlayGameModel : PageModel
+namespace WebApp.Pages.Play
 {
-    public required string GameName { get; set; }
-    
-    public void OnGet(string gameName)
+    public class PlayGameModel : PageModel
     {
-        GameName = gameName;
+        [BindProperty(SupportsGet = true)]
+        public required string GameName { get; set; }
+        
+        [BindProperty(SupportsGet = true)]
+        public string? Password { get; set; }
+        
+        public void OnGet(string gameName, string? password)
+        {
+            GameName = gameName;
+            Password = password;
+        }
+        
+        public IActionResult OnPost(string password)
+        {
+            Password = password;
+
+            return Page();
+        }
     }
 }
--- TicTacTwo/WebApp/Pages/Play/Setup.cshtml.cs
+++ TicTacTwo/WebApp/Pages/Play/Setup.cshtml.cs
@@ -1,25 +1,105 @@
-using Data.Models.db;
+using Common.Entities;
+using Data.Repositories;
+using GameLogic;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
-namespace WebApp.Pages.Play;
-
-public class Setup(Data.Context.GameDbContext context) : PageModel
+namespace WebApp.Pages.Play
 {
-    [BindProperty]
-    public SaveGame SaveGame { get; set; } = default!;
-
-    public async Task<IActionResult> OnPostAsync()
+    public class SetupModel(IGameRepository gameRepository, IConfigRepository configRepository) : PageModel
     {
-        if (!ModelState.IsValid)
+        [BindProperty]
+        public required string GameName { get; set; }
+
+        [BindProperty]
+        public required string ConfigurationName { get; set; }
+
+        [BindProperty]
+        public string? PasswordP1 { get; set; }
+
+        [BindProperty]
+        public string? PasswordP2 { get; set; }
+
+        public required SelectList ConfigurationNames { get; set; }
+
+        public async Task OnGetAsync()
         {
-            return Page();
+            await LoadConfigurationNamesAsync();
         }
 
-        context.SavedGames.Add(SaveGame);
-        await context.SaveChangesAsync();
+        public async Task<IActionResult> OnPostAsync()
+        {
+            await LoadConfigurationNamesAsync();
+            if (!ModelState.IsValid) return Page();
 
-        return RedirectToPage("./Index");
+            var errorMessage = GameConfigurationValidator.ValidateInputAsAlphanumeric(GameName);
+            if (!string.IsNullOrEmpty(errorMessage))
+            {
+                ModelState.AddModelError(string.Empty, errorMessage);
+                return Page();
+            }
+
+            if (gameRepository.GetSavedGamesNamesAsync().Result.Contains(GameName))
+            {
+                ModelState.AddModelError(string.Empty, "Game with that name already exists");
+                return Page();
+            }
+
+            GameConfiguration selectedConfiguration;
+            try
+            {
+                selectedConfiguration = await configRepository.GetConfigurationByNameAsync(ConfigurationName);
+            }
+            catch
+            {
+                ModelState.AddModelError(string.Empty, "Invalid game configuration.");
+                return Page();
+            }
+
+            if (selectedConfiguration.Mode is EGameMode.SinglePlayer && PasswordP1 == null)
+            {
+                ModelState.AddModelError(string.Empty, "Single player mode requires password for player 1.");
+                return Page();
+            }
+            
+            if (selectedConfiguration.Mode is EGameMode.OnlineTwoPlayer && (PasswordP1 == null || PasswordP2 == null))
+            {
+                ModelState.AddModelError(string.Empty, "Online Two player mode requires passwords for both players.");
+                return Page();
+            }
+
+            if (PasswordP1 != null)
+            {
+                errorMessage = GameConfigurationValidator.ValidateInputAsAlphanumeric(PasswordP1);
+                if (!string.IsNullOrEmpty(errorMessage))
+                {
+                    ModelState.AddModelError(string.Empty, errorMessage);
+                    return Page();
+                }
+            }
+
+            if (PasswordP2 != null)
+            {
+                errorMessage = GameConfigurationValidator.ValidateInputAsAlphanumeric(PasswordP2);
+                if (!string.IsNullOrEmpty(errorMessage))
+                {
+                    ModelState.AddModelError(string.Empty, errorMessage);
+                    return Page();
+                }
+            }
+
+            var gameState = new GameState(selectedConfiguration);
+
+            var newGame = new Game(GameName, selectedConfiguration, gameState, PasswordP1, PasswordP2);
+            await gameRepository.SaveNewGameAsync(newGame);
+
+            return RedirectToPage("/Play/Index", new { gameName = GameName });
+        }
+
+        private async Task LoadConfigurationNamesAsync()
+        {
+            ConfigurationNames = new SelectList(await configRepository.GetConfigurationNamesAsync());
+        }
     }
 }
